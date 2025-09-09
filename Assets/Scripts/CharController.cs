209a527cb9680490d90434a6233d61d63bc7d63f
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-09-09 22:45:24 +0300

    Setup for the game and make thge character move

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private bool walkRight = true;
+	// Use this for initialization
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime;
+	}
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Switch();
+		}
+	}
+    void Switch(){
+		walkRight = !walkRight;
+		if (walkRight)
+		{
+			transform.rotation = Quaternion.Euler(0f, 45f, 0f);
+		}
+		else
+		{
+			transform.rotation = Quaternion.Euler(0, -45, 0);
+		}
+	}	
+}
