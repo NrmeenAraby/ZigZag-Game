3963de3029794d33b1fd4492bf78e1db521e0592
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-09-09 23:13:35 +0300

    Added script to make the camera follows the character

---
+++ Assets/Scripts/CamFollowsChar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowsChar : MonoBehaviour {
+
+	public Transform target;
+	private Vector3 offset;
+	// Use this for initialization
+	void Awake () {
+		offset = transform.position-target.position;
+	}
+	void LateUpdate()
+	{
+		transform.position = target.position+offset;
+	}
+	
+}
